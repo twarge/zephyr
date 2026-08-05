595b03201391a7fe944008b54d46963312d28828
Tom Kornack <tom@twarge.com>
2026-08-05 07:59:48 -0400

    Drop the try2 closure for a flattened try? in the image guard

--- Packages/ZulipKit/Sources/ZulipContent/ContentParser.swift
+++ Packages/ZulipKit/Sources/ZulipContent/ContentParser.swift
@@ -241,7 +241,7 @@ public enum ContentParser {
             // Tolerant of wrapper variation: the anchor need not be the
             // first child, nor the img a direct child of the anchor.
             guard let anchor = element.children().first(where: { $0.tagName() == "a" }),
-                  let img = try2({ try anchor.select("img").first() }) ?? nil
+                  let img = try? anchor.select("img").first()
             else {
                 return .unimplemented(html: outerHTML(element))
             }
